509500afa6dd51b172ae942528d0efd81358f74e
Sam Meech-Ward <sam@meech-ward.me>
2022-01-12 10:30:49 -0800

    Add the front end code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require("express")
 const multer = require('multer')
 const fs = require('fs')
+const path = require("path")
 
 const database = require('./database')
 
@@ -10,9 +11,7 @@ const app = express()
 
 const upload = multer({ dest: 'images/' })
 
-app.get("/", (req, res) => {
-  res.send("💩")
-})
+app.use(express.static(path.join(__dirname, "build")))
 
 app.get('/images/:imageName', (req, res) => {
   const imageName = req.params.imageName
